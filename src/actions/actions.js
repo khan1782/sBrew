24b609cb9dc659bff5e95218df314d65ec1f8ebc
kevin <khan1782@gmail.com>
2017-05-20 21:32:11 -0700

    ASD

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -13,14 +13,28 @@ export function sortEvents(category){ return {type: SORT_EVENTS, category}}
 export function filterEvents(category){return {type: FILTER_EVENTS, category}}
 export function sortLocations(category){return {type: SORT_LOCATIONS, category}}
 export function filterLocations(category){return {type: FILTER_LOCATIONS, category}}
-
 export function showBrewsAll(){return {type: SHOW_BREWS_ALL}}
 export function showBrewModal(uuid){return {type: SHOW_BREW_MODAL, uuid}}
 export function showBrewSquare(uuid){return {type: SHOW_BREW_SQUARE, uuid}}
-
 export function showEventsAll(){return {type: SHOW_EVENTS_ALL}}
 export function showEventSquare(id){return {type: SHOW_EVENT_SQUARE, id}}
 export function showEventModal(id){return {type:SHOW_EVENT_MODAL}}
+
+
+export const TOGGLE_EVENTS_FEED_VISIBILITY = "TOGGLE_EVENTS_FEED_VISIBILITY"
+export const TOGGLE_EVENT_MODAL_VISIBILITY = "TOGGLE_EVENT_MODAL_VISIBILITY"
+export const TOGGLE_BREWS_FEED_VISIBILITY = "TOGGLE_BREWS_FEED_VISIBILITY"
+export const TOGGLE_BREW_MODAL_VISIBILITY = "TOGGLE_BREW_MODAL_VISIBILITY"
+export const TOGGLE_LOCATIONS_FEED_VISIBILITY = "TOGGLE_LOCATIONS_FEED_VISIBILITY"
+export const TOGGLE_LOCATIONS_MODAL_VISIBILITY = "TOGGLE_LOCATIONS_MODAL_VISIBILITY"
+
+export function toggleEventsFeedVisibility(){return {type:TOGGLE_EVENTS_FEED_VISIBILITY}}
+export function toggleEventModalVisibility(){return {type:TOGGLE_EVENT_MODAL_VISIBILITY}}
+export function toggleBrewsFeedVisibility(){return {type:TOGGLE_BREWS_FEED_VISIBILITY}}
+export function toggleBrewModalVisibility(){return{type:TOGGLE_BREW_MODAL_VISIBILITY}}
+export function toggleLocationFeedVisibility(){return {type:TOGGLE_LOCATIONS_FEED_VISIBILITY}}
+export function toggleLocationModalVisibility(){return {type:TOGGLE_LOCATIONS_MODAL_VISIBILITY}}
+
 
 export const EventFilters = {
 	DATE:"DATE",
--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ import App from './App';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import {SHOW_BREWS_ALL,SHOW_BREW_SQUARE,SHOW_BREW_MODAL,SHOW_EVENTS_ALL,SHOW_EVENT_SQUARE,SHOW_EVENT_MODAL,SORT_EVENTS,FILTER_EVENTS,SORT_LOCATIONS,FILTER_LOCATIONS} from './actions/actions'
+import {sortEvents,filterEvents,sortLocations,filterLocations,showBrewsAll,showBrewModal,showBrewSquare,showEventsAll,showEventSquare,showEventModal,EventFilters,EventSorts,LocationSorts,FilterSorts} from './actions/actions'
+import {TOGGLE_EVENTS_FEED_VISIBILITY,TOGGLE_EVENT_MODAL_VISIBILITY,TOGGLE_BREWS_FEED_VISIBILITY,TOGGLE_BREW_MODAL_VISIBILITY,TOGGLE_LOCATIONS_FEED_VISIBILITY,TOGGLE_LOCATIONS_MODAL_VISIBILITY} from './actions/actions'
+import {toggleEventsFeedVisibility,toggleEventModalVisibility,toggleBrewsFeedVisibility,toggleBrewModalVisibility,toggleLocationFeedVisibility,toggleLocationModalVisibility} from './actions/actions'
 
 
+console.log(sortEvents("ASD"))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
