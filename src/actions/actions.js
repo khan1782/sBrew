a18cee0087bbcf24d9c31e3f8558c54af26b487d
kevin <khan1782@gmail.com>
2017-05-21 02:47:58 -0700

    ASD

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,52 +1,38 @@
-export const SHOW_BREWS_ALL = "SHOW_BREWS_ALL"
-export const SHOW_BREW_SQUARE = "SHOW_BREW_SQUARE"
-export const SHOW_BREW_MODAL = "SHOW_BREW_MODAL"
-export const SHOW_EVENTS_ALL = "SHOW_EVENTS_ALL"
-export const SHOW_EVENT_SQUARE = "SHOW_EVENT_SQUARE"
-export const SHOW_EVENT_MODAL = "SHOW_EVENT_MODAL"
-export const SORT_EVENTS = "SORT_EVENTS"
-export const FILTER_EVENTS = "FILTER_EVENTS"
-export const SORT_LOCATIONS = "SORT_LOCATIONS"
-export const FILTER_LOCATIONS = "FILTER_LOCATIONS"
-
+export function toggleVisibility(targetComponent){return {type:TOGGLE_VISIBILITY, component:targetComponent}}
 export function sortEvents(category){ return {type: SORT_EVENTS, category}}
 export function filterEvents(category){return {type: FILTER_EVENTS, category}}
 export function sortLocations(category){return {type: SORT_LOCATIONS, category}}
 export function filterLocations(category){return {type: FILTER_LOCATIONS, category}}
-export function showBrewsAll(){return {type: SHOW_BREWS_ALL}}
-export function showBrewModal(uuid){return {type: SHOW_BREW_MODAL, uuid}}
-export function showBrewSquare(uuid){return {type: SHOW_BREW_SQUARE, uuid}}
-export function showEventsAll(){return {type: SHOW_EVENTS_ALL}}
-export function showEventSquare(id){return {type: SHOW_EVENT_SQUARE, id}}
-export function showEventModal(id){return {type:SHOW_EVENT_MODAL}}
+
+export const SORT_EVENTS = "SORT_EVENTS"
+export const FILTER_EVENTS = "FILTER_EVENTS"
+export const SORT_LOCATIONS = "SORT_LOCATIONS"
+export const FILTER_LOCATIONS = "FILTER_LOCATIONS"
+export const TOGGLE_VISIBILITY = "TOGGLE_VISIBILITY"
 
 
-export const TOGGLE_EVENTS_FEED_VISIBILITY = "TOGGLE_EVENTS_FEED_VISIBILITY"
-export const TOGGLE_EVENT_MODAL_VISIBILITY = "TOGGLE_EVENT_MODAL_VISIBILITY"
-export const TOGGLE_BREWS_FEED_VISIBILITY = "TOGGLE_BREWS_FEED_VISIBILITY"
-export const TOGGLE_BREW_MODAL_VISIBILITY = "TOGGLE_BREW_MODAL_VISIBILITY"
-export const TOGGLE_LOCATIONS_FEED_VISIBILITY = "TOGGLE_LOCATIONS_FEED_VISIBILITY"
-export const TOGGLE_LOCATIONS_MODAL_VISIBILITY = "TOGGLE_LOCATIONS_MODAL_VISIBILITY"
-
-export function toggleEventsFeedVisibility(){return {type:TOGGLE_EVENTS_FEED_VISIBILITY}}
-export function toggleEventModalVisibility(){return {type:TOGGLE_EVENT_MODAL_VISIBILITY}}
-export function toggleBrewsFeedVisibility(){return {type:TOGGLE_BREWS_FEED_VISIBILITY}}
-export function toggleBrewModalVisibility(){return{type:TOGGLE_BREW_MODAL_VISIBILITY}}
-export function toggleLocationFeedVisibility(){return {type:TOGGLE_LOCATIONS_FEED_VISIBILITY}}
-export function toggleLocationModalVisibility(){return {type:TOGGLE_LOCATIONS_MODAL_VISIBILITY}}
-
-
-export const EventFilters = {
-	DATE:"DATE",
-	TAG:"TAG"
+export const EventFilters = {	DATE:"DATE",TAG:"TAG"}
+export const EventSorts = {DATE:"DATE",LOCATION:"LOCATION"}
+export const LocationSorts = {LOCATION:"LOCATION"}
+export const FilterSorts = {TAG:"TAG"}
+export const VisibleComponents = {
+	EVENT_MODAL:"EVENT_MODAL",
+	EVENT_FEED:"EVENT_FEED",
+	BREW_MODAL:"BREW_MODAL",
+	BREW_FEED:"BREW_FEED",
+	LOCATION_MODAL:"LOCATION_MODAL",
+	LOCATION_FEED:"LOCATION_FEED"
 }
-export const EventSorts = {
-	DATE:"DATE",
-	LOCATION:"LOCATION"
-}
-export const LocationSorts = {
-	LOCATION:"LOCATION"
-}
-export const FilterSorts = {
-	TAG:"TAG"
+
+export const EventTags={
+	OTHER:"Other" ,
+	BEER_PROMOTIONS:"Beer Promotions",
+	SPORTS_AND_ACTIVITIES:"Sports & Activities",
+	BEER_RELEASES:"Beer Releases",
+	CHARITY_EVENTS:"Charity Events",
+	FOOD_PAIRINGS:"Food Pairings",
+	SPECIAL_CASKS:"Special Casks",
+	BEER_FESTIVALS:"Beer Festivals",
+	EDUCATION:"Education" ,
+	SPECIAL_TAPS:"Special Taps"
 }
