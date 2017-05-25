2c6bbfcd2d408c2aabc14ee2cb04be3246c8e8d5
kevin <khan1782@gmail.com>
2017-05-24 22:46:41 -0700

    ASD

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import beerApp from './reducers' 
 import App from './App';
 
